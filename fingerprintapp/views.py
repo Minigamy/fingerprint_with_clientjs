c73a4bde10db5ab6d173e5625d294d46ace4b6b4
Minigamy <ferdinand-f@mail.ru>
2022-02-04 12:32:59 +0300

    add proxy ip

--- fingerprintapp/views.py
+++ fingerprintapp/views.py
@@ -16,6 +16,11 @@ class Fingerprint(View):
         proxy_ip3 = str(request.headers.get('X-Forwarded-Host'))
         proxy_ip4 = str(request.headers.get('X-Forwarded-Proto'))
         proxy_ip5 = str(request.headers.get('Via'))
+        proxy_ip6 = str(request.headers.get('Proxy-Authenticate'))
+        proxy_ip7 = str(request.headers.get('Proxy-Authentication-Info'))
+        proxy_ip8 = str(request.headers.get('Proxy-Authorization'))
+        proxy_ip9 = str(request.headers.get('Proxy-Connection'))
+        proxy_ip10 = str(request.headers.get('X-Request-URI'))
 
         if request_ip in data.tor_ip_list:
             is_tor = True
@@ -32,6 +37,11 @@ class Fingerprint(View):
                                'proxy_ip3': proxy_ip3,
                                'proxy_ip4': proxy_ip4,
                                'proxy_ip5': proxy_ip5,
+                               'proxy_ip6': proxy_ip6,
+                               'proxy_ip7': proxy_ip7,
+                               'proxy_ip8': proxy_ip8,
+                               'proxy_ip9': proxy_ip9,
+                               'proxy_ip10': proxy_ip10,
                                'is_tor': is_tor,
                                'is_vpn': ipinfo['vpn'],
                                'is_proxy': ipinfo['proxy']}
