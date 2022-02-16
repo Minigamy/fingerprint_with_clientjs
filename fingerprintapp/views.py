ceaf12b9c04348ec78aee9fad2bef7073ecd2fec
Minigamy <ferdinand-f@mail.ru>
2022-02-16 12:27:04 +0300

    restart

--- fingerprintapp/views.py
+++ fingerprintapp/views.py
@@ -16,7 +16,7 @@ class Fingerprint(View):
             is_tor = True
 
         # vpnapi_info = (requests.get(f'https://vpnapi.io/api/{request_ip}?key=5d458b25a61345cdb4ed78563b82836c')).json()
-        ipinfo = (requests.get(f'https://ipinfo.io/{request_ip}/privacy?token=861f5ef47b8113')).json()
+        ipinfo = (requests.get(f'https://ipinfo.io/{request_ip}/privacy?token=f93bf0f8fecddb')).json()
         # ipdata = requests.get(
         #         f'https://api.ipdata.co/{request_ip}?api-key=2992ccd498bab4e86af5763df63bd10d7bf5f32665f3d26b68e3c823')
 
